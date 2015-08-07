346eb61590c1096093780365af383cab64ff0c42
Jorge Gallardo <jorge8486@gmail.com>
2015-08-07 11:41:53 -0400

    added volume, scrub, and button functionality

--- Back To Bach/ViewController.swift
+++ Back To Bach/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var scrubSlider: UISlider!
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+                scrubSlider.maximumValue = Float(player.duration)
+        } catch {
+            // it didn't work
+        }
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateScrubSlider"), userInfo: nil, repeats: true)
     }
 
+    @IBAction func adjustVolume(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+
+    @IBAction func scrub(sender: AnyObject) {
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+    }
+    
+    func updateScrubSlider() {
+        scrubSlider.value = Float(player.currentTime)
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        player.volume = volumeSlider.value
+        player.play()
+    }
+
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        player.pause()
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+        } catch {
+            // it didn't work
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
